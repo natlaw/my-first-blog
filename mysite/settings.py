fb133c10a194519289063402e4cf6fe536ca0c8d
natlaw <natashalawrance@hotmail.com>
2017-12-09 11:59:53 +0000

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jofh))!qtpgx!kwp)42a_m#!^54rr)un&7lrg%ga3oojac%)3r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '<natlaw>.pythonanywhere.com']
 
